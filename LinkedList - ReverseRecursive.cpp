e982bd2c9119d0383662b2038b649983475c0655
Hashirzz <86047099+Hashirzz@users.noreply.github.com>
2021-08-22 21:55:50 +0530

    Create LinkedList - ReverseRecursive.cpp

---
+++ LinkedList - ReverseRecursive.cpp
@@ -0,0 +1,22 @@
+Node *reverseLinkedListRec(Node *head)
+{
+    //Write your code here
+    if(head == NULL || head -> next == NULL){
+        return head;
+    }
+    
+    Node *smallans = reverseLinkedListRec(head -> next);
+    
+    Node *temp = smallans;
+    
+    while(temp -> next != NULL){
+        temp = temp -> next;
+    }
+    
+    temp -> next = head;
+    head -> next = NULL;
+    
+    return smallans;
+    
+    
+}
