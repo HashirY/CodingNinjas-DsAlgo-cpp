79ac81e08f444205b9f56f7f41f54c3b4c451479
Hashirzz <86047099+Hashirzz@users.noreply.github.com>
2021-08-25 11:47:15 +0530

    Create SumOfNodes.cpp

---
+++ Trees/SumOfNodes.cpp
@@ -0,0 +1,33 @@
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+
+int sumOfNodes(TreeNode<int>* root) {
+    // Write your code here
+    int sum = root -> data;
+    
+    for(int i =0;i<root -> children.size();i++){
+        sum += sumOfNodes(root -> children[i]);
+    }
+    
+    return sum;
+}
