aedf463dc8508bcac48723e77538c0a5bc1ee567
Hashirzz <86047099+Hashirzz@users.noreply.github.com>
2021-08-26 12:51:01 +0530

    Create PreOrderBT.cpp

---
+++ BinaryTree/PreOrderBT.cpp
@@ -0,0 +1,29 @@
+/**********************************************************
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+
+void preOrder(BinaryTreeNode<int> *root) {
+    
+    if(root == NULL){
+        return;
+    }
+	// Write your code here
+    cout<<root -> data<<" ";
+    preOrder(root -> left);
+    preOrder(root -> right);
+}
